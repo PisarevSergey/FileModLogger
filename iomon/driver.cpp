291921d16b1123edaa18873b35156bb17afd421f
sergey pisarev <pisarev@tssltd.ru>
2019-05-03 00:52:21 +0300

    load on boot

--- iomon/driver.cpp
+++ iomon/driver.cpp
@@ -17,7 +17,7 @@ namespace
   }
 
   NTSTATUS attach(_In_ PCFLT_RELATED_OBJECTS    /*FltObjects*/,
-                  _In_ FLT_INSTANCE_SETUP_FLAGS /*Flags*/,
+                  _In_ FLT_INSTANCE_SETUP_FLAGS Flags,
                   _In_ DEVICE_TYPE              VolumeDeviceType,
                   _In_ FLT_FILESYSTEM_TYPE      /*VolumeFilesystemType*/)
   {
@@ -25,8 +25,16 @@ namespace
 
     if (FILE_DEVICE_DISK_FILE_SYSTEM == VolumeDeviceType)
     {
-      im(DRIVER, "volume device type is disk file system, attaching");
-      stat = STATUS_SUCCESS;
+      im(DRIVER, "volume device type is disk file system");
+      if (Flags & FLTFL_INSTANCE_SETUP_NEWLY_MOUNTED_VOLUME)
+      {
+        im(DRIVER, "new volume, attaching");
+        stat = STATUS_SUCCESS;
+      }
+      else
+      {
+        im(DRIVER, "not a new volume, skipping attach");
+      }
     }
     else
     {
